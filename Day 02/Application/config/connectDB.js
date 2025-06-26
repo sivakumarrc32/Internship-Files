36fe326b509119b79aea59e58769a4ef1f61e5c1
sivakumarrc32 <sivakumarrconline@gmail.com>
2025-06-26 10:49:34 +0530

    Delete Day 02/Application directory

--- Day 02/Application/config/connectDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB =  () => {
-    mongoose.connect(process.env.DB_URL)
-       .then((con) => console.log('MongoDB Connected to Host :'+con.connection.host))
-       .catch((err) => console.log(err));
-}
-
-
-module.exports = connectDB;
